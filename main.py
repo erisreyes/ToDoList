bb099e9679b3c470f85651df827a4ef6869b1d0d
erisreyes <eris.reyes@media-meter.com>
2023-02-15 15:24:52 +0800

    make a UI using tkinter

--- main.py
+++ main.py
@@ -1,81 +1,39 @@
-import json
+import tkinter as tk
 
-# Define the file name
-filename = "tasks.json"
+# Create a window
+window = tk.Tk()
+window.title("To-Do List App")
 
-# Load the tasks from the file
-try:
-    with open(filename, "r") as f:
-        tasks = json.load(f)
-except FileNotFoundError:
-    tasks = []
+# Create a label for the input field
+input_label = tk.Label(window, text="Enter a task:")
+input_label.pack()
 
-def view_task():
-    # View all the tasks in the list
-    print("Tasks:")
-    for i, task in enumerate(tasks):
-        print(f"{i+1}. {task}")
+# Create an input field
+input_field = tk.Entry(window)
+input_field.pack()
 
+# Create a listbox to display tasks
+listbox = tk.Listbox(window)
+listbox.pack()
 
-# Create a loop that allows the user to add, edit, or delete tasks
-while True:
-    # Display options to the user
-    print("1. Add task")
-    print("2. Edit task")
-    print("3. Delete task")
-    print("4. View tasks")
-    print("5. Exit")
+# Define a function to add a task to the listbox
+def add_task():
+    task = input_field.get()
+    listbox.insert(tk.END, task)
+    input_field.delete(0, tk.END)
 
-    choice = input("Enter your choice: ")
+# Create a button to add tasks
+add_button = tk.Button(window, text="Add task", command=add_task)
+add_button.pack()
 
-    # Handle user's choice
-    if choice == "1":
-        # Add a new task to the list
-        task = input("Enter task: ")
-        tasks.append(task)
+# Create a function to remove a task from the listbox
+def remove_task():
+    task_index = listbox.curselection()[0]
+    listbox.delete(task_index)
 
-        # Write the tasks to the file
-        with open(filename, "w") as f:
-            json.dump(tasks, f)
+# Create a button to remove tasks
+remove_button = tk.Button(window, text="Remove task", command=remove_task)
+remove_button.pack()
 
-        print('\n********')
-        view_task()
-    elif choice == "2":
-        # Edit an existing task in the list
-        index = int(input("Enter task number: ")) - 1
-        new_task = input("Enter new task: ")
-        tasks[index] = new_task
-
-        # Write the tasks to the file
-        with open(filename, "w") as f:
-            json.dump(tasks, f)
-    elif choice == "3":
-        view_task()
-
-        # Delete an existing task from the list
-        print('\n')
-        index = int(input("Enter task number: ")) - 1
-        del tasks[index]
-
-        # Write the tasks to the file
-        with open(filename, "w") as f:
-            json.dump(tasks, f)
-    elif choice == "4":
-        view_task()
-
-        # Ask the user if they want to display options again
-        print('\n********')
-        display_options = input("Do you want to display options again? (y/n): ")
-        if display_options.lower() == "y":
-            print('\n')
-            continue
-        else:
-            break
-    elif choice == "5":
-        # Exit the loop
-        break
-    else:
-        print("Invalid choice. Please try again.")
-
-
-
+# Start the main event loop
+window.mainloop()
